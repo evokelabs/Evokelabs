9e415c6340ff664a30bbbdd5c72f1f6ef35761dc
Adrian Pikios <adrian@evokelabs.com>
2022-10-29 15:08:09 +1100

    404 centered

--- wp-content/themes/evokelabs2013/404.php
+++ wp-content/themes/evokelabs2013/404.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?>
-
-            <div id="about">
+<div id="about-container">
+            <div id="about" style="margin-top: 5rem;">
             	 <h3>File Not Found</h3>
             	 <h1 style="text-align: center;">ಠ_ಠ</h1>
             </div>  <!-- about -->
+            </div> <!-- about-container -->
            </div> <!-- darkger bg-->
            </div> <!-- darkger bg-->
 <?php get_footer(); ?> 
